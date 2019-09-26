f759b3e9c877d32561fb3a001f5ac983c9e6ed55
Patryk Wydmuch <181745@centrala.bzwbk>
2019-09-26 15:50:06 +0200

    Add bootstrap

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,14 +1,36 @@
-const arabForm = document.querySelector('.to-arabic')
+const form = document.querySelector('.my-form')
+const toArabicRadio = document.querySelector('#to-arabic')
+const toRomanRadio = document.querySelector('#to-roman')
+const enterInput = document.querySelector('#enter-input')
+const result = document.querySelector('#result')
+let currentDestination = 'arabska'
 
-arabForm.addEventListener('submit',event => {
+form.addEventListener('submit',event => {
     event.preventDefault()
-    fetch("http://localhost:9090/arabska/I")
-        .then(resp => resp.json())
+    const number = enterInput.value
+//    const dest = currentDestination
+    console.log(`http://localhost:9090/${currentDestination}/${number}`)
+    fetch(`http://localhost:9090/${currentDestination}/${number}`)
+        .then(resp => resp.text())
         .then(resp => {
-            console.log(resp)
+        console.log(resp)
+
+            result.value=resp
         })
         .catch(err =>{
             console.log(err)
         })
 })
+
+toRomanRadio.addEventListener('click',()=>{
+console.log('switch to roman')
+currentDestination='rzymska'
+})
+toArabicRadio.addEventListener('click',()=>{
+
+console.log('switch to arab')
+currentDestination='arabska'
+})
+
+
 
