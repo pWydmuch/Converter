9c7334e6dc8522fd1e3191bdcda7523af502a400
Patryk Wydmuch <pvydmuch@gmail.com>
2019-09-29 18:58:26 +0200

    Add google cloud support

--- src/main/java/com/example/converter/ConverterApplication.java
+++ src/main/java/com/example/converter/ConverterApplication.java
@@ -4,17 +4,25 @@ import com.example.converter.service.ConverterToArabic;
 import com.example.converter.service.ConverterToRoman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
-public class ConverterApplication {
+public class ConverterApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConverterApplication.class, args);
     }
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConverterApplication.class);
+    }
+	
     @Bean //how to change scope in @Component
     @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public ConverterToRoman getConverterToRoman(){ //name of the method was important
