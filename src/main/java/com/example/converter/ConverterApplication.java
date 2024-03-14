6cab6224b1fa1ad9dc219e7e0321feea15003c6e
Patryk Wydmuch <pvydmuch@gmail.com>
2024-03-14 15:32:13 +0100

    Clean up frontend side a bit

--- src/main/java/com/example/converter/ConverterApplication.java
+++ src/main/java/com/example/converter/ConverterApplication.java
@@ -10,23 +10,5 @@ public class ConverterApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ConverterApplication.class, args);
     }
-	
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(ConverterApplication.class);
-//    }
-//
-//    @Bean //how to change scope in @Component
-//    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
-//    public ConverterToRoman getConverterToRoman(){ //name of the method was important
-//        return new ConverterToRoman();
-//    }
-//
-//    @Bean
-//    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
-//    public ConverterToArabic getConverterToArabic(){
-//        return new ConverterToArabic();
-//    }
-
 }
 
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -4,26 +4,27 @@ const toRomanRadio = document.querySelector('#to-roman')
 const enterInput = document.querySelector('#enter-input')
 const result = document.querySelector('#result')
 const alert = document.querySelector('.alert')
-let currentDestination = 'arabic-equiv'
+let currentTarget = 'arabic-equiv'
 
 form.addEventListener('submit', event => {
     event.preventDefault()
     const number = enterInput.value
-    // fetch(`http://localhost:8080/${currentDestination}?number=${number}`)
-    fetch(`https://simple-converter.herokuapp.com/${currentDestination}?number=${number}`)
+    // fetch(`http://localhost:8080/${currentTarget}?number=${number}`)
+    fetch(`https://simple-converter.herokuapp.com/${currentTarget}?number=${number}`)
         .then((response) => {
             if (response.ok) fillResult(response.text())
             else showErrorMessage(response.text())
         })
 })
 
-const fillResult = (responsePromise) => {
+const fillResult = responsePromise => {
     alert.style.display = 'none'
     responsePromise.then(resp => {
         result.value = resp
     })
 }
-const showErrorMessage = (responsePromise) => {
+
+const showErrorMessage = responsePromise => {
     responsePromise.then(resp => {
         alert.style.display = 'block'
         alert.innerText = resp
@@ -32,17 +33,16 @@ const showErrorMessage = (responsePromise) => {
 
 toRomanRadio.addEventListener('click', () => {
     result.value = ""
-    enterInput.value= ""
+    enterInput.value = ""
     enterInput.focus()
-    alert.style.display = 'none'    
-    currentDestination = 'roman-equiv'
+    alert.style.display = 'none'
+    currentTarget = 'roman-equiv'
 })
-
 
 toArabicRadio.addEventListener('click', () => {
     result.value = ""
     enterInput.value = ""
     enterInput.focus()
     alert.style.display = 'none'
-    currentDestination = 'arabic-equiv'
+    currentTarget = 'arabic-equiv'
 })
