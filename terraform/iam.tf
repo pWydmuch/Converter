5cc08a9f65ed4e0c9fb0c11f7ebb410003286732
Patryk Wydmuch <pvydmuch@gmail.com>
2025-05-31 19:37:38 +0200

    Build and deploy front

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,16 +23,31 @@ resource "aws_iam_role" "github_actions" {
   })
 }
 
-resource "aws_iam_role_policy" "s3_read" {
-  name = "s3-read"
+resource "aws_iam_role_policy" "s3_access" {
+  name = "s3-access"
   role = aws_iam_role.github_actions.id
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = "s3:GetObject"
-      Resource = "arn:aws:s3:::${aws_s3_bucket.frontend_config.id}/api-url.txt"
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.frontend_config.id}/api-url.txt"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::p11h-converter",
+          "arn:aws:s3:::p11h-converter/*"
+        ]
+      }
+    ]
   })
 }
 
