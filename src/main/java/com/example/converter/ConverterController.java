7b4b5a4319d0eb89ab9bbd4dd2e9aee5c2abe671
Patryk Wydmuch <pvydmuch@gmail.com>
2025-06-29 21:50:22 +0200

    Make it work as intended

--- src/main/java/com/example/converter/ConverterController.java
+++ src/main/java/com/example/converter/ConverterController.java
@@ -19,8 +19,7 @@ public class ConverterController {
     final private ConverterToArabic converterToArabic;
 
     final private Jedis jedis;
-    static final private String TO_ARABIC_LIST = "recent:roman_to_arabic";
-    private static final String TO_ROMAN_LIST = "recent:arabic_to_roman";
+    private static final String RECENTS_LIST_NAME = "recents";
 
     public ConverterController(ConverterToRoman converterToRoman, ConverterToArabic converterToArabic, Jedis jedis) {
         this.converterToRoman = converterToRoman;
@@ -32,8 +31,8 @@ public class ConverterController {
     public Integer getConvertedToArab(@RequestParam("number") String romanNumber) {
         Integer result = converterToArabic.convert(romanNumber);
         String value = romanNumber.toUpperCase() + "=" + result;
-        jedis.lpush(TO_ARABIC_LIST, value);
-        jedis.ltrim(TO_ARABIC_LIST, 0, 4);
+        jedis.lpush(RECENTS_LIST_NAME, value);
+        jedis.ltrim(RECENTS_LIST_NAME, 0, 4);
         return result;
     }
 
@@ -41,40 +40,18 @@ public class ConverterController {
     public String getConvertedToRoman(@RequestParam("number") Integer arabicNumber) {
         String result = converterToRoman.convert(arabicNumber);
         String value = arabicNumber + "=" + result;
-        jedis.lpush(TO_ROMAN_LIST, value);
-        jedis.ltrim(TO_ROMAN_LIST, 0, 4);
+        jedis.lpush(RECENTS_LIST_NAME, value);
+        jedis.ltrim(RECENTS_LIST_NAME, 0, 4);
         return result;
     }
 
-    @GetMapping("/recents-to-arabic")
-    public Map<String, Integer> getRecentToArabicConversions() {
-        List<String> recentConversions = jedis.lrange(TO_ARABIC_LIST, 0, 4);
+    @GetMapping("/recents")
+    public List<List<String>> getRecentConversions() {
+        List<String> recentConversions = jedis.lrange(RECENTS_LIST_NAME, 0, 4);
         return recentConversions.stream()
-                .map(s -> s.split("="))
-                .collect(
-                        Collectors.toMap(
-                                parts -> parts[0],
-                                parts -> Integer.parseInt(parts[1]),
-                                (existing, replacement) -> existing,
-                                LinkedHashMap::new
-                        )
-                );
-
-    }
-
-    @GetMapping("/recents-to-roman")
-    public Map<Integer, String> getRecentToRomanConversions() {
-        List<String> recentConversions = jedis.lrange(TO_ROMAN_LIST, 0, 4);
-        return recentConversions.stream()
-                .map(s -> s.split("="))
-                .collect(
-                        Collectors.toMap(
-                                parts -> Integer.parseInt(parts[0]),
-                                parts -> parts[1],
-                                (existing, replacement) -> existing,
-                                LinkedHashMap::new
-                        )
-                );
+                .map(conversion -> conversion.split("="))
+                .map(parts -> List.of(parts[0], parts[1]))
+                .toList();
 
     }
 
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,9 +1,11 @@
 const form = document.querySelector('.my-form')
 const toArabicRadio = document.querySelector('#to-arabic')
 const toRomanRadio = document.querySelector('#to-roman')
+const last5Checkbox = document.querySelector('#last-5')
 const enterInput = document.querySelector('#enter-input')
 const result = document.querySelector('#result')
 const alert = document.querySelector('.alert')
+const recentList = document.querySelector('#recent-list')
 let currentTarget = 'arabic-equiv'
 
 form.addEventListener('submit', event => {
@@ -13,6 +15,9 @@ form.addEventListener('submit', event => {
         .then((response) => {
             if (response.ok) fillResult(response.text())
             else showErrorMessage(response.text())
+            if (last5Checkbox.checked) {
+                fetchRecents();
+            }
         })
 })
 
@@ -45,3 +50,28 @@ toArabicRadio.addEventListener('click', () => {
     alert.style.display = 'none'
     currentTarget = 'arabic-equiv'
 })
+
+last5Checkbox.addEventListener('click', e => {
+    if (e.target.checked) {
+        fetchRecents();
+
+    } else {
+        recentList.innerHTML = ''
+    }
+})
+
+function fetchRecents() {
+    fetch(`http://localhost:8080/recents`)
+        .then((response) => {
+            return response.json()
+        })
+        .then(json => {
+            recentList.innerHTML = ''
+            for (const [key, value] of json) {
+                const li = document.createElement('li');
+                li.textContent = `${key} ⟶ ${value}`;
+                recentList.appendChild(li);
+            }
+            console.log(json)
+        })
+}
